8c7a442a7f2608beb9959a8f5d60321863516052
linwei09824 <15883778755@163.com>
2026-03-04 10:14:56 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,9 +1,7 @@
 const CONFIG = {
-    titleWeb: "【你的网站标题】", // 例："写给XX的告白"
-    introTitle: '【暗恋对象的名字】', // 例："小敏"
-    introDesc: `【自定义开头文案】
-    例：晚风轻踩云朵，月亮贩卖温柔
-    而我偷偷把你，藏进心底角落`,
+    titleWeb: "写给王玥大宝贝的告白", // 例："写给XX的告白"
+    introTitle: '玥玥大baby', // 例："小敏"
+    introDesc: `今夜星光闪闪，爱你的心满满
     btnIntro: '^^打开告白^^',
     title: '是不是第一眼就心动啦 🥰',
     desc: '是不是看到我的笑容就沦陷啦 ',
@@ -13,7 +11,7 @@ const CONFIG = {
     btnReply: '发送心意 <3',
     reply: '喜欢就是喜欢，没有理由 💘',
     mess: '我早就知道啦 🥰. 超爱你的 😘😘',
-    messDesc: '【你的邀约】', // 例："今晚8点，我在楼下等你，一起去看星星～"
+    messDesc: '快点见面好不好', // 例："今晚8点，我在楼下等你，一起去看星星～"
     btnAccept: '好耶好耶 <3',
     messLink: '【你的消息链接】' // 例："https://m.me/123456789"（Facebook私信）、微信/QQ链接等
 }
